a9dfc2b8e0330a1a8dac9c54a3fabae9b1da911b
Pedro Leon <pedrolbbcarvalho@gmail.com>
2024-03-13 10:11:04 -0300

    create token refresh

--- banking_api/src/main/java/com/banking_api/banking_api/infra/security/TokenService.java
+++ banking_api/src/main/java/com/banking_api/banking_api/infra/security/TokenService.java
@@ -5,6 +5,8 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.banking_api.banking_api.domain.user.User;
+import com.banking_api.banking_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -14,8 +16,14 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
+    private final UserRepository userRepository;
 
-    public String tokenGenerate(User user) {
+    public TokenService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public String tokenGenerate(UserDetails user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256("123");
             return JWT.create()
@@ -26,9 +34,25 @@ public class TokenService {
         } catch (JWTCreationException exception) {
             throw new RuntimeException("erro ao  gerar o token", exception);
         }
+
+
+
+
+
     }
 
     public String getSubject(String tokenJWT) {
+        tokenJWT = tokenJWT.trim();
+
+        // Verifica se a string termina com \r\n e remove se necessário
+        if (tokenJWT.endsWith("\r\n")) {
+            tokenJWT = tokenJWT.substring(0, tokenJWT.length() - 2);
+        }
+
+        // Verifica se a string está entre aspas e remove se necessário
+        if (tokenJWT.startsWith("\"") && tokenJWT.endsWith("\"")) {
+            tokenJWT = tokenJWT.substring(1, tokenJWT.length() - 1);
+        }
         try {
             Algorithm algorithm = Algorithm.HMAC256("123");
             return JWT.require(algorithm)
@@ -45,5 +69,13 @@ public class TokenService {
             return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
 
         }
+
+    public String refreshToken(String tokenJWT) {
+
+        String subject = getSubject(tokenJWT);
+        var user = userRepository.findByUsername(subject);
+
+        return tokenGenerate(user);
+    }
     }
 
